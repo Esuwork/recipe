0fc49454a51e1fe6593683fccc8d6775b3ae757a
unknown <enersuworks@gmail.com>
2025-05-27 20:11:24 +0800

    search icon-iig gargana

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,40 @@
 require("@babel/polyfill");
 import Search from "./models/Search";
+import { elements, renderLoader, clearLoader } from "./view/base";
+import * as searchView from './view/searchView';
+//  WEB APP- iin tuluv
+//  Hailtin query, ur dun
+//  Tuhain uzuulj baigaa jor
+//  Like-lsan joruud
+//  Zahialr baigaa jorin nairlaganuud
 
-let search = new Search('pasta');
+const state = {};
 
-search.doSearch().then(r => console.log(r));
+const controlSearch = async () => {
+    // 1) Web-s hailtin tulhuur ugiig gargaj avna
+    const query = searchView.getInput();
+
+    if(query) {
+    // 2) Shineer hailtin obektiig uusgej ugnu
+    state.search = new Search(query);
+    // 3) Hail hiihed zoriulj delgetsiig beltgene
+        searchView.clearSearchQuery();
+        searchView.clearSearchResult();
+
+        renderLoader(elements.searchResultDiv);
+
+    // 4) Hailtiig guitsetgene
+    await state.search.doSearch();
+
+    // 5) Hailtin ur dung delgetsen uzuulne..
+    clearLoader();
+    if (state.search.result === undefined) alert(`Hailtaar ilertsgui...`);
+    else searchView.renderRecipes(state.search.result);
+    }
+
+};
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,20 +1,18 @@
 require("@babel/polyfill");
 import axios from "axios";
+
 export default class Search {
-    constructor() {
-        this.query = this.query;
+    constructor(query) { // Accept query as a parameter
+        this.query = query; // Set this.query to the passed query
     }
     
-async doSearch() {
-    try {
-    let result = await axios('https://forkify-api.herokuapp.com/api/search?q=pizza#'+ this.query);
-
-    this.result = result.data.recipes;
-
-    return this.result;
-
-    } catch (error) {
-        alert('error zaasan ' + error);
+    async doSearch() {
+        try {
+            let result = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`); // Use this.query in the API call
+            this.result = result.data.recipes;
+            return this.result;
+        } catch (error) {
+            console.log('Error occurred: ' + error);
+        }
     }
-};
-};
+}
